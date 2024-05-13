798730092f91c6a30e97b5f6d7c925fc95e93010
ASKalkas <96704211+ASKalkas@users.noreply.github.com>
2024-05-14 00:22:03 +0300

    deploy

--- Server/app.js
+++ Server/app.js
@@ -9,6 +9,10 @@ const authRouter = require("./Routes/authRoutes");
 const authenticationMiddleware = require("./Middleware/authenticationMiddleware");
 require('dotenv').config();
 
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname  , "../client/dist");
+
+
 const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,16 +20,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 
 app.use(
-    cors({
-      origin: process.env.ORIGIN,
-      methods: ["GET", "POST", "DELETE", "PUT"],
-      credentials: true,
-    })
-  );
+  cors({
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(express.static(buildPath));
+
+app.get("/*", function(req, res){
+
+  res.sendFile(
+      path.join(__dirname, "../client/build/index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+
+})
 
 app.use("/api/v1", authRouter);
 app.use(authenticationMiddleware);
 app.use("/api/v1/list", listRouter);
 app.use("/api/v1/users", userRouter);
 
-app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
+app.listen(5001, () => console.log('Server is running on http://localhost:5001'));
